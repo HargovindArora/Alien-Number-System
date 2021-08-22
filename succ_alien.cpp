8df6c103e908ecded7a499502691d18cc825a6e1
HargovindArora <hargovinds50@gmail.com>
2021-08-22 22:37:42 +0530

    Tried to made the code more modular and the pieces of code more independent of each other wherever possible by breaking the big 'succ_alien' function into multiple, small independent functions with proper returning types

--- succ_alien.cpp
+++ succ_alien.cpp
@@ -3,9 +3,7 @@
 
 using namespace std;
 
-void succ_alien(string n, string b){
-
-    int length = n.size()-1, base = b.size();
+vector<int> getNumFromString(string n, string b, int length, int base){
 
     vector<int> num;
 
@@ -17,6 +15,11 @@ void succ_alien(string n, string b){
             }
         }
     }
+
+    return num;
+}
+
+void incrementBy1(vector<int> &num, int length, int base){
 
     int carry = 1;
 
@@ -31,11 +34,29 @@ void succ_alien(string n, string b){
         }
     }
 
+    return;
+}
+
+string convertNumToString(vector<int> &num, string b){
+
     string result = "";
 
     for(int n: num){
         result += b[n];
     }
+
+    return result;
+}
+
+void succ_alien(string n, string b){
+
+    int length = n.size()-1, base = b.size();
+
+    vector<int> num = getNumFromString(n, b, length, base);
+
+    incrementBy1(num, length, base);
+
+    string result = convertNumToString(num, b);
 
     cout << result << endl;
 
